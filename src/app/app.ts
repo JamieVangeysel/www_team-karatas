23d9507133acef980d1a6235513158e3650c6acb
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2025-07-31 15:48:33 +0200

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ka-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'team-karatas';
+}
